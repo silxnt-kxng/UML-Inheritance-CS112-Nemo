62bad5cf8206550724f8f7f58897796aa40f08a7
schoolaccount5 <losalas@dons.usfca.edu>
2025-09-25 14:07:00 -0700

    Update TwoDimensionalShape.java
    test copy

--- TwoDimensionalShape.java
+++ TwoDimensionalShape.java
@@ -1,3 +1,18 @@
 public class TwoDimensionalShape {
-	
+	private double borderWidth;
+
+	public TwoDimensionalShape(Point center, String color, boolean filled, double borderWidth){
+		super(center, color, filled);
+		this.borderWidth = borderWidth;
+	}
+
+	public setBorderWidth(double input){
+		borderWidth = input;
+	}
+
+	public getBorderWidth(){
+		return borderWidth;
+	}
+
+	public void 
 }
