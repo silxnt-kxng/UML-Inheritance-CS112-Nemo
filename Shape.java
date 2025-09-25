ed08694739283cf3d858598babcb56b03ede449f
enzoqueirolo <ejqueirolo@dons.usfca.edu>
2025-09-25 13:44:33 -0700

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -1,3 +1,28 @@
-public class Shape {
-	h
+public abstract class Shape {
+	protected Point center;
+	protected String color;
+	protected boolean filled;
+
+	public void Shape(Point center, String color, boolean filled) {
+		this.center = center;
+		this.color = color;
+		this.filled = filled;
+	}
+
+	public Point getCenter() {
+		return this.center;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public abstract calculateArea();
+	public abstract calcuatePerimeter();
+	public abstract draw();
+
+	public void move(Point) {
+		
+	}
+
 }
