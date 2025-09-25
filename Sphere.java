0f2ba78452753cb278e9d9329dcf047df4d49f40
Z <silverlining0013@gmail.com>
2025-09-25 14:11:16 -0700

    Finish Sphere class

--- Sphere.java
+++ Sphere.java
@@ -1,3 +1,26 @@
 public class Sphere {
-	
+	private double radius;
+
+    public Sphere(double radius){
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateVolume(){
+        return (4/3)*Math.PI*(radius*radius*radius);
+    }
+
+    @Override
+    public double calculateSurfaceArea(){
+        return 4*Math.PI*(radius*radius);
+    }
+
+    @Override
+    public void draw(){
+        System.out.println("I'm in the draw method for the Sphere class");
+    }
+
+    public void setRadius(double radius){
+        this.radius = radius;
+    }
 }
